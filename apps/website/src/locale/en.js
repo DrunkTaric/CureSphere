3d820c2a7864efbd5a2cfcbb5cb48c97c1bcbd16
ComboRush <comborush.init@proton.me>
2024-04-15 23:28:03 +0200

    translations  (WIP)

---
+++ apps/website/src/locale/en.js
@@ -0,0 +1,76 @@
+export default {
+    "navigation": {
+        "home": "Home",
+        "about": "About",
+        "contact": "Contact",
+        "appointments": "Departments",
+        "forum": "Forum",
+        "futures": "Futures",
+        "reviews": "Reviews",
+        "login": "Login",
+        "signup": "Signup"
+    },
+    "home": {
+        "title": "Home",
+        "description": "Home",
+        "expolre-button": "Explore",
+        "label": "Welcome to our hospital"
+    },
+    "signup": {
+        "title": "Signup",
+        "description": "Signup",
+        "name": "Name",
+        "civil-id": "Civil ID",
+        "email": "Email",
+        "password": "Password",
+        "confirm": "Confirm",
+        "submit": "Submit"
+    },
+    "login": {
+        "title": "Login",
+        "description": "Login",
+        "email": "Email",
+        "password": "Password",
+        "submit": "Submit"
+    },
+    "about": {
+        "title": "About",
+        "description": "About",
+        "content": "Welcome to Reave Hospital, a beacon of healthcare excellence since its establishment in 1970 by the visionary Ahmed Zewail. Inspired by a commitment to innovation and compassionate care, Dr. Zewail envisioned a medical institution that would seamlessly blend cutting-edge technology with a human-centric approach. Over the decades, Reave Hospital has evolved into a leading healthcare provider, continually raising the bar in medical standards. Our founder's pioneering spirit remains embedded in our ethos, driving us to adopt the latest advancements in healthcare to ensure our patients receive the best possible treatment.From our humble beginnings to becoming a cornerstone of the community, Reave Hospital has consistently prioritized affordability without compromising on quality. Our commitment to providing accessible and comprehensive healthcare has earned us the trust of generations. At Reave Hospital, the legacy of Ahmed Zewail lives on through our dedicated healthcare professionals and state-of-the-art facilities. We look forward to continuing our journey of healing, innovation, and service for many more years to come.",
+        "metadata-1": "Happy Patients",
+        "metadata-2": "Surgeries",
+        "metadata-3": "Endoscopy"
+    },
+    "contact": {
+        "title": "Contact",
+        "description": "Contact"
+    },
+    "footer": {
+        "copyright": "All rights reserved",
+        "made": "Made by 301 team"
+    },
+    "forum": {
+        "title": "Forum",
+        "description": "Forum",
+        "start-post-label": "Start Post",
+        "show-post": "Show",
+        "ask-ai": "Ask AI",
+        "comment-here-label": "Comment Here",
+        "user-type-1": "Patient",
+        "user-type-2": "Doctor"
+    },
+    "futures": {
+        "title": "Futures",
+        "description": "Futures",
+        "first-future":"Enjoy peace of mind with our 24/7 availability, ensuring quality healthcare whenever you need it for continuous support and care.",
+        "second-future": "Your safety is paramount. We employ advanced security measures to ensure a protected environment throughout your experience with us.",
+        "third-future": "Access comprehensive care with state-of-the-art tests and advanced equipment, ensuring accurate results for personalized healthcare tailored to your needs.",
+        "forth-future": "",
+        "fifth-future": "Discover top-notch medical care paired with excellent insurance coverage for your peace of mind and comprehensive well-being.",
+        "sixth-future": "Experience advanced orthopedic care with the latest technology at affordable prices, prioritizing both innovation and your financial well-being."
+    },
+    "reviews": {
+        "title": "Reviews",
+        "description": "Reviews"
+    }
+}
