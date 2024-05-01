98c5a7e2bbabf7a077a5058f145eac351cb049ca
DrunkTaric <comborush.init@proton.me>
2024-05-01 23:35:36 +0300

    made the navbar server sided

--- apps/web/app/layout.tsx
+++ apps/web/app/layout.tsx
@@ -1,7 +1,7 @@
 import type { Metadata } from "next";
+import Nav from "@/components/blocks/Nav";
 import { Inter } from "next/font/google";
 import "./globals.css";
-import Nav from "@/components/blocks/Nav";
 
 const inter = Inter({ subsets: ["latin"] });
 
