b01d148d2c8e4193aa5a79c3026cce3703e98151
DrunkTaric <comborush.init@proton.me>
2024-05-01 23:46:48 +0300

    removing useless stuff

--- apps/web/components/ui/font.tsx
+++
@@ -1,8 +0,0 @@
-"use client"
-
-import Font from "react-font"
-import { Label } from "./label"
-
-export default function FontLabel({ text, family, className }: { text: string, family: string, className?: string }) {
-  return <Font family={family}><Label className={className}>{text}</Label></Font>
-}
