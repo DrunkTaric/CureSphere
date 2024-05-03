d9ffa88f0953bcc01e6d30e05ea663e5947fe2c4
DrunkTaric <comborush.init@proton.me>
2024-05-03 22:36:29 +0300

    added some cool functions such as parse URL prams

---
+++ apps/web/utils/requester.ts
@@ -0,0 +1,23 @@
+import { convertToSource } from "./converter"
+
+export function extractParams<T>(url: string): T {
+  const local_url = new URL(url)
+  const local_params = new URLSearchParams(local_url.search)
+  let object: { [key: string]: string | number | boolean | object } = {}
+
+  const local_entries = local_params.entries()
+  let current_entry = local_entries.next()
+
+  while (!current_entry.done) {
+    object[current_entry.value[0]] = convertToSource(current_entry.value[1])
+    current_entry = local_entries.next()
+  }
+
+  return object as T
+}
+
+export function Parse() {
+}
+
+export function Request() {
+}
